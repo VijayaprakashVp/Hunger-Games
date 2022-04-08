25a4ca436bc5f32d01dea50712f4a6ce823e6284
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-08 09:03:12 +0530

    Finsihed

--- Hunger-Games/src/App.jsx
+++ Hunger-Games/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { Buttons } from "./components/Buttons";
 import { RestaurantDetails } from "./components/RestaurantDetails";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Buttons />
-      <RestaurantDetails />
+      <Routes>
+        <Route path="/" element={<RestaurantDetails />} />
+        <Route path="/all" element={<RestaurantDetails />} />
+      </Routes>
     </div>
   );
 }
--- Hunger-Games/src/components/RestaurantDetails.jsx
+++ Hunger-Games/src/components/RestaurantDetails.jsx
@@ -1,20 +1,143 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export function RestaurantDetails() {
   const [data, setData] = useState([]);
+  const [show, setShow] = useState([]);
+  console.log("show:", show);
+
   const url = "http://localhost:8080/get-restaurants";
 
   useEffect(() => {
     axios.get(url).then((res) => {
       //   console.log(res.data);
+      setShow(res.data);
       setData(res.data);
     });
   }, []);
 
+  // Handle Rating
+
+  const handleRating = (val) => {
+    let tem = show.filter((e) => {
+      if (e.rating >= +val) {
+        return e;
+      }
+    });
+    setShow(tem);
+  };
+
+  // Cost for two sorting
+
+  const handleCostSort = (cost) => {
+    if (cost == 0) {
+      let tem = [...show];
+      tem.sort((a, b) => a.cost_for_two - b.cost_for_two);
+      setShow(tem);
+    }
+    if (cost == 1) {
+      let tem = [...show];
+      tem.sort((a, b) => b.cost_for_two - a.cost_for_two);
+      setShow(tem);
+    }
+  };
+
+  // Payment Sorting
+
+  const handlePayment = (met) => {
+    if (met == "cash") {
+      let tem = show.filter((e) => {
+        if (e.payment_method.cash) {
+          return e;
+        }
+      });
+      setShow(tem);
+    }
+    if (met == "card") {
+      let tem = show.filter((e) => {
+        if (e.payment_method.card) {
+          return e;
+        }
+      });
+      setShow(tem);
+    }
+  };
+
   return (
     <div>
-      {data.map((e) => {
+      <div className="buttons">
+        <div className="page">
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
+        <div className="rating">
+          <button
+            onClick={() => {
+              handleRating(4);
+            }}
+          >
+            4 and Above
+          </button>
+          <button
+            onClick={() => {
+              handleRating(3);
+            }}
+          >
+            3 and Above
+          </button>
+          <button
+            onClick={() => {
+              handleRating(2);
+            }}
+          >
+            2 and Above
+          </button>
+          <button
+            onClick={() => {
+              handleRating(1);
+            }}
+          >
+            1 and Above
+          </button>
+        </div>
+        <div className="payment">
+          <button
+            onClick={() => {
+              handlePayment("cash");
+            }}
+          >
+            Cash
+          </button>
+          <button
+            onClick={() => {
+              handlePayment("card");
+            }}
+          >
+            Card
+          </button>
+          <Link to={"/all"}>
+            <button>All</button>
+          </Link>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              handleCostSort(0);
+            }}
+          >
+            Cost - Low to High
+          </button>
+          <button
+            onClick={() => {
+              handleCostSort(1);
+            }}
+          >
+            Cost - High to Low
+          </button>
+        </div>
+      </div>
+      {show.map((e) => {
         return (
           <div className="overall">
             <div>
@@ -24,6 +147,7 @@ export function RestaurantDetails() {
               <p>Name : {e.name}</p>
               <p>Category : {e.category}</p>
               <p>Cost for Two : {e.cost_for_two}</p>
+              {/* <p>Payment Method : {e.payment_method.cash} {e.payment_method.card}</p> */}
             </div>
             <div className="review">
               <p>Rating : {e.rating}</p>
